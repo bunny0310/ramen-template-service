eb1c40826a9514677a0f85e7b0913149309bc438
Ishaan Khurana <khuranaishaan5@gmail.com>
2021-03-23 16:15:54 -0400

    03/23/21 replated _id with userId in getTemplates method

--- controller.js
+++ controller.js
@@ -1,13 +1,13 @@
 const {Template} = require('./models/template');
 
 const getTemplates = async (id) => {
-        const templates = await Template.find({"_id": id});
+        const templates = await Template.find({"userId": id});
         return templates;
 }
 
 const insertTemplate = async (body) => {
         const template = new Template({'template': body.template, 'alias': body.alias, 'userId': body.userId});
-        template.save();
+        await template.save();
         return template;
 };
 
